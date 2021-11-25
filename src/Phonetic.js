275dcd2d5406a434c05d96c0062b1c394388a50c
EmiBusso <emiliabusso@gmail.com>
2021-11-24 16:03:30 -0800

    Styled the application

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props){
     return (
     <div className="Phonetic">
+        <span className="text">
         {props.phonetic.text}
-        <br />
+        </span>
         <a href={props.phonetic.audio} target="_blank">Listen</a>
     </div>
     );
